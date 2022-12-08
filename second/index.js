bc9f894c0b40eb5659475b7105fd2a7a87439029
Gautam-2002 <gautam2sethi@gmail.com>
2022-12-08 14:27:04 +0530

    get vs post,post data encode,restfull api CRUD

--- second/index.js
+++ second/index.js
@@ -30,31 +30,99 @@
 // templating language/engine that lets its user generate HTML 
 // with plain javascript. EJS is mostly useful whenever you 
 // have to output HTML with a lot of javascript. 
+
+// const express = require('express');
+// const app = express();
+// app.set('view engine','ejs')
+
+// app.get('/',(req,res)=>{
+//     res.render('index')
+// })
+// // app.get('/random',(req,res)=>{
+// //     const data = Math.floor(Math.random()*100)
+// //     res.render('random',{data})
+// // })
+// app.get("/random", (req, res) => {
+//   const data = ['delhi','shimla','solan'];
+//   res.render("random", { data });
+// });
+// app.get("/form", (req, res) => {
+//   res.render("form");
+// });
+// app.get("/user", (req, res) => {
+// //   console.log(req.query)
+//   const {user_name,age}=req.query;
+//   res.send(`${user_name} and ${age}`);
+// });
+// const port = 2424
+// app.listen(port,()=>{
+//     console.log(`server running on ${port}`)
+
+// })
+
+
+
+
+// date - 08-12-2022
+
+// const express = require('express');
+// const app = express();
+// app.use(express.urlencoded({extended:true}))
+// app.set('view engine','ejs');
+// app.get('/',(req,res)=>{
+//   res.send('Good morning Mr. Sethi');
+// })
+// app.get("/user", (req, res) => {
+//   res.render('form');
+// });
+// app.get("/data", (req, res) => {
+//   res.send("got all data");
+// });
+// app.post("/data", (req, res) => {
+//   console.log(req.body)
+//   res.send("got all data from post");
+// });
+// app.listen(4444,()=>{
+//   console.log('server running on 4444');
+// })
+
+
+// Restfull API - provide architecture(syntax) to write code(good code).
+// Restfull API used for CRUD operations.
+
 const express = require('express');
 const app = express();
-app.set('view engine','ejs')
+app.use(express.urlencoded({extended:true}))
+const arr = [
+  {
+    user: "one",
+    code: "white",
+  },
+  {
+    user: "two",
+    code: "grey",
+  },
+  {
+    user: "three",
+    code: "black",
+  },
+];
+app.set('view engine','ejs');
 
 app.get('/',(req,res)=>{
-    res.render('index')
+    res.send(" it's a fine day")
 })
-// app.get('/random',(req,res)=>{
-//     const data = Math.floor(Math.random()*100)
-//     res.render('random',{data})
-// })
-app.get("/random", (req, res) => {
-  const data = ['delhi','shimla','solan'];
-  res.render("random", { data });
-});
-app.get("/form", (req, res) => {
-  res.render("form");
-});
-app.get("/user", (req, res) => {
-//   console.log(req.query)
-  const {user_name,age}=req.query;
-  res.send(`${user_name} and ${age}`);
-});
-const port = 2424
-app.listen(port,()=>{
-    console.log(`server running on ${port}`)
 
+app.get('/code',(req,res)=>{
+    res.render('index',{arr})
+})
+app.get("/code/new", (req, res) => {
+  res.render('new');
+});
+app.post("/code", (req, res) => {
+  arr.push(req.body)
+  res.redirect('/code');
+});
+app.listen(4444,()=>{
+    console.log('server running fine')
 })
