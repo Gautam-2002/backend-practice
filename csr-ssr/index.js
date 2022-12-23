aecb2fd21dec0b59b452e17f387cb4b0f24e7aae
Gautam-2002 <gautam2sethi@gmail.com>
2022-12-23 10:25:22 +0530

    sending data from client to server

--- csr-ssr/index.js
+++ csr-ssr/index.js
@@ -3,12 +3,13 @@ const app = express();
 
 app.use(express.static('public'))//this is a middleware function
 app.set('view engine','ejs')
+app.use(express.urlencoded({extended:true}))
 const arr = ['-1','0','1']
 
 app.get('/todo',(req,res)=>{
     if (req.xhr) {
         // client side rendering 
-        console.log('this is ajax req')
+        // console.log('this is ajax req')
         res.json(arr)
     }
     else{
@@ -17,6 +18,12 @@ app.get('/todo',(req,res)=>{
         res.render('index',{arr})
     }
     // res.send("hello, mr. sethi !!")
+})
+app.post('/todo',(req,res)=>{
+    // console.log(req.body)
+    const {todo} = req.body
+    arr.push(todo)
+    res.redirect('/todo')
 })
 app.listen(3000,()=>{
     console.log('server running on port no. 3000')
--- csr-ssr/public/index.js
+++ csr-ssr/public/index.js
@@ -1,7 +1,25 @@
-console.log(jQuery);
-$.get('/todo',(data)=>{
-    // console.log(data);
-    for (let todo of data) {
-        $('#list').append(`<li> ${todo}</li>`)
-    }
+// console.log(jQuery);
+const inp = document.querySelector('#inp')
+const btn = document.querySelector('#btn')
+
+function refresh() {
+    $.get('/todo',(data)=>{
+        // console.log(data);
+        for (let todo of data) {
+            $('#list').append(`<li> ${todo}</li>`)
+        }
+    })
+}
+refresh()
+
+btn.addEventListener('click',()=>{
+    // console.log('it works !!');
+    const todotxt = inp.value;
+    $.post('/todo',{todo:todotxt},()=>{
+        console.log('works well !!')
+    })
+    inp.value = "";
+    // document.querySelector('#list').innerHTML = "";
+    $('#list').empty();
+    refresh()
 })
